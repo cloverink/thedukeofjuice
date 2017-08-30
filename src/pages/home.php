012184900d286848b489bdfb8885f09ebba6156a
cloverink <cloverink@gmail.com>
2017-08-30 16:42:17 +0700

    section 3 product list

--- src/pages/home.php
+++ src/pages/home.php
@@ -3,12 +3,12 @@
 <?php include 'section-desc.php' ?>
 
 <section id="section-items" class="">
-  <a href="#!"></a>
-  <a href="#!"></a>
-  <a href="#!"></a>
-  <a href="#!"></a>
-  <a href="#!"></a>
-  <a href="#!"></a>
+  <a href="#!" class="brown"><img src="/assets/images/1x1.png"></a>
+  <a href="#!" class="green"><img src="/assets/images/1x1.png"></a>
+  <a href="#!" class="white"><img src="/assets/images/1x1.png"></a>
+  <a href="#!" class="red"><img src="/assets/images/1x1.png"></a>
+  <a href="#!" class="orange"><img src="/assets/images/1x1.png"></a>
+  <a href="#!" class="yellow"><img src="/assets/images/1x1.png"></a>
 </section>
 
 <section>
