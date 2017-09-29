1535b413027b6c437e4066d191b0eb3eba2bec3e
cloverink <cloverink@gmail.com>
2017-09-29 23:51:38 +0700

    toggle main nav

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,9 +1,30 @@
 var Main = (function (self) {
   var my = self, func = {};
 
-  my.init = function () {
-  	$(document).foundation();
+
+
+  func.initMainNav = function() {
+    $("#lnkMainNav").on("click", function(e){
+      e.stopPropagation();
+      $(this).toggleClass("actived");
+    })
+    $("#lnkMainNav").parent().on("click", function(e){
+      e.stopPropagation();
+      $("#lnkMainNav").removeClass("actived");
+    });
+  }
+
+  func.init = function() {
+    func.initMainNav();
+  }
+
+
+  my.init = function() {
+    $(document).ready(function(){
+      func.init();
+    });
   };
 
   return my;
-})(Main || {});
+
+})(Main || {}).init();
--- src/partials/header.php
+++ src/partials/header.php
@@ -5,6 +5,14 @@
 <nav>
   <div>
     <a id="lnkMainNav" href="#!" class=""></a>
+    <div id="navPopupMenu">
+      <a id="lnkHome" href="#home">Home</a>
+      <a id="lnkAbout" href="#about">about</a>
+      <a id="lnkAccount" href="#account">account</a>
+      <a id="lnkContact" href="#contact">contact</a>
+      <a id="lnkShopping" href="#shopping-cart">shopping cart</a>
+      <a id="lnkStore" href="#store-location">store location</a>
+    </div>
   </div>
   <div>
     <a id="lnkLogo" href="#!">back to top</a>
