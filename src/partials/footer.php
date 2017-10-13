f80989878c5c2adbaecd9958e434fec02194d039
cloverink <cloverink@gmail.com>
2017-10-13 19:10:14 +0700

    edit footer

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -1,7 +1,7 @@
 <footer>
 
-  <a href="#!"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-  <a href="#!"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-  <a href="#!"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+  <a href="http://facebook.com" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+  <a href="http://twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+  <a href="http://instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 
 </footer>
