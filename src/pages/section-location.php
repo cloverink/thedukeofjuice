8e3307209494d71a3453fbbfd5de90b5b379306e
cloverink <cloverink@gmail.com>
2017-10-13 19:21:54 +0700

    edit map

--- src/pages/section-location.php
+++ src/pages/section-location.php
@@ -3,11 +3,11 @@
   <div class="container">
 
     <div>
-      <iframe class="map" frameborder="0" style="border:0" 
-      src="https://www.google.com/maps/embed/v1/place?key=<?=$site_google_map_api?>&q=260+Clarendon+St,+South+Melbourne+VIC+3205" allowfullscreen>
+      <iframe 
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.1917024899954!2d144.95792031588647!3d-37.832397742884005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad667ffcb8fe0eb%3A0x68b8dc026b263aae!2zMjYwIENsYXJlbmRvbiBTdCwgU291dGggTWVsYm91cm5lIFZJQyAzMjA1IOC4reC4reC4quC5gOC4leC4o-C5gOC4peC4teC4og!5e0!3m2!1sth!2sth!4v1507897233557" 
+      frameborder="0" style="border:0" allowfullscreen></iframe>
       </iframe>
     </div>
-
     <div>
       <img src="/assets/images/store-front.png">
 
